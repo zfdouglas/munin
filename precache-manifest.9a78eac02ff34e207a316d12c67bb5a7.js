0a3fac36cbe851d143ef52eb600c135c0e7378c0
ZFinn <puppybees@gmail.com>
2020-07-20 17:25:35 -0400

    Updates

---
+++ precache-manifest.9a78eac02ff34e207a316d12c67bb5a7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa47b621ea254f80b1e70f0dd2f09bf2",
+    "url": "/munin/index.html"
+  },
+  {
+    "revision": "4f78e6746e8f86b11980",
+    "url": "/munin/static/css/main.8dcfc786.chunk.css"
+  },
+  {
+    "revision": "4cd62c1fee07d8bfe12c",
+    "url": "/munin/static/js/2.4f7aff9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/munin/static/js/2.4f7aff9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f78e6746e8f86b11980",
+    "url": "/munin/static/js/main.a882c2d2.chunk.js"
+  },
+  {
+    "revision": "2bd3ec1fd8e31d778b9b",
+    "url": "/munin/static/js/runtime-main.d67cee50.js"
+  },
+  {
+    "revision": "14b52b7c6d1a4d13c0315ec44be75cca",
+    "url": "/munin/static/media/cupmine2.14b52b7c.svg"
+  },
+  {
+    "revision": "71f69670b4708fad6dca6c85cc67066f",
+    "url": "/munin/static/media/pentaclemine2.71f69670.svg"
+  },
+  {
+    "revision": "3b7c4e9a0be62b30ff569445d5c6649a",
+    "url": "/munin/static/media/ravengif.3b7c4e9a.gif"
+  },
+  {
+    "revision": "07e47b8a6aebc34d2b5abfc4c5dcd89f",
+    "url": "/munin/static/media/ravenlines2w.07e47b8a.svg"
+  },
+  {
+    "revision": "a16e0a449bf3e692879b6f7ac6d73743",
+    "url": "/munin/static/media/swordmine2.a16e0a44.svg"
+  },
+  {
+    "revision": "14ed98796baf78777384bc390675169d",
+    "url": "/munin/static/media/tarot2.14ed9879.svg"
+  },
+  {
+    "revision": "f17d6d8b3ae7afbc17cb3507cf00aeb2",
+    "url": "/munin/static/media/triquetra.f17d6d8b.svg"
+  },
+  {
+    "revision": "a626c1bf1a6f1526bc89dcc2f1d7637d",
+    "url": "/munin/static/media/wandsmine2.a626c1bf.svg"
+  }
+]);
